ad9f48f41ffbdcc04055b681dc5603c33eb8d380
Oscar Olsson <happybits@gmail.com>
2021-10-12 11:54:52 +0200

    -

---
+++ CSharpCourse/CSharpCourse/Linq/Solutions/Show.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CSharpCourse.Linq.Solutions
+{
+    class Show
+    {
+        public string Channel { get; set; }
+        public TimeSpan StartAt { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ CSharpCourse/CSharpCourse/Linq/Solutions/TvShows.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCourse.Linq.Solutions
+{
+    public class TvShows
+    {
+        public static void Run()
+        {
+            string[] rows = ReadTvShowFile();
+
+            IEnumerable<Show> allShows = ParseTvShows(rows);
+
+            DisplayInfoAboutShows(allShows);
+
+            // Extrauppgift, gör en funktion "ParseTvShows_Linq"
+        }
+
+        private static void DisplayInfoAboutShows(IEnumerable<Show> allShows)
+        {
+
+            // Skriv ut alla programtitlar
+
+            Console.WriteLine("ALLA TITLAR");
+            Console.WriteLine();
+
+            var titlar = allShows.Select(s => s.Title);
+
+            foreach(var titel in titlar)
+            {
+                Console.WriteLine(titel);
+            }
+
+            // Alla program som börjar senare än 21
+
+            // todo: Fortsätt koda här!
+        }
+
+        private static IEnumerable<Show> ParseTvShows(string[] rows)
+        {
+            var allShows = new List<Show>();
+
+            foreach (string line in rows)
+            {
+                /*
+                   "line" kan t.ex se ut såhär:
+                   SVT1*22:00*Fatta Sveriges demokrati
+                */
+                string[] splittedLine = line.Split('*');
+
+                var show = new Show
+                {
+                    Channel = splittedLine[0],
+                    StartAt = TimeSpan.Parse(splittedLine[1]),
+                    Title = splittedLine[2],
+                };
+
+                allShows.Add(show);
+            }
+            return allShows;
+        }
+
+        private static string[] ReadTvShowFile()
+        {
+            return File.ReadAllLines(@"Linq\Solutions\tv-data.txt");
+        }
+    }
+}
