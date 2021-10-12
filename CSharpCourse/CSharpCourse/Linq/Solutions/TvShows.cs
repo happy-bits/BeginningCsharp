6ce81551c19670ca91c424d86a79e57c73fd5daa
Oscar Olsson <happybits@gmail.com>
2021-10-12 13:48:57 +0200

    -

--- CSharpCourse/CSharpCourse/Linq/Solutions/TvShows.cs
+++ CSharpCourse/CSharpCourse/Linq/Solutions/TvShows.cs
@@ -1,43 +1,154 @@
-﻿using System;
+﻿/*
+ Syfte: hyfsat verkligt exempel 
+
+ Läs in en textfil och vrid och vänd på datan:
+ - Filtrera
+ - Manipulera
+ - Sortera
+
+ I detta fall en lista med tv-shower
+ 
+ */
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSharpCourse.Linq.Solutions
 {
+
     public class TvShows
     {
         public static void Run()
         {
             string[] rows = ReadTvShowFile();
 
-            IEnumerable<Show> allShows = ParseTvShows(rows);
+            var allShows = ParseTvShows(rows);
+
+            // Extrauppgift, lös ParseTvShow med ett linq-uttryck (one-liner)
+            // var allShows = ParseTvShows_Linq(rows);
 
             DisplayInfoAboutShows(allShows);
 
-            // Extrauppgift, gör en funktion "ParseTvShows_Linq"
+        }
+
+        // Extrauppgift
+
+        private static string[] ReadTvShowFile()
+        {
+            return File.ReadAllLines(@"Linq\Solutions\tv-data.txt");
         }
 
         private static void DisplayInfoAboutShows(IEnumerable<Show> allShows)
         {
+            // Alla programtitlar
 
-            // Skriv ut alla programtitlar
+            Header("Alla titlar");
 
-            Console.WriteLine("ALLA TITLAR");
-            Console.WriteLine();
-
-            var titlar = allShows.Select(s => s.Title);
-
-            foreach(var titel in titlar)
+            foreach (string title in allShows.Select(s => s.Title))
             {
-                Console.WriteLine(titel);
+                Console.WriteLine(title);
             }
 
-            // Alla program som börjar senare än 21
+            // Alla program som börjar senare än 21.00 - alternativ I
 
-            // todo: Fortsätt koda här!
+            Header("Program som börjar senare än kl 21");
+
+            var laterThan21 = new List<Show>();
+
+            foreach (Show show in allShows)
+            {
+                if (show.StartAt.Hours >= 21)
+                {
+                    laterThan21.Add(show);
+                }
+            }
+
+            foreach (Show show in laterThan21)
+            {
+                WriteInfoAboutShow(show);
+            }
+
+            // Alla program som börjar senare än 21.00 - alternativ II (Linq)
+
+            Header("Program som börjar senare än kl 21 - med Linq");
+
+            foreach (Show show in allShows.Where(show => show.StartAt >= TimeSpan.FromHours(21)))
+            //foreach (Show show in allShows.Where(show => show.StartAt >= TimeSpan.Parse("21:00")))
+            //foreach (Show show in allShows.Where(show => show.StartAt.Hours >= 21))
+            {
+                WriteInfoAboutShow(show);
+            }
+
+            // Alla program som börjar senare än 21.00 - alternativ III (Linq och foreach)
+
+            Header("Program som börjar senare än kl 21 - med Linq och foreach");
+
+            allShows.Where(show => show.StartAt.Hours >= 21).ToList().ForEach(show =>
+                WriteInfoAboutShow(show)
+            );
+
+            // Program från SVT2 i kronologisk ordning
+
+            Header("Program från svt2 i kronologisk ordning");
+
+            foreach (Show show in allShows.Where(x => x.Channel == "SVT2").OrderBy(x => x.StartAt))
+            {
+                WriteInfoAboutShow(show);
+            }
+
+            Header("Finns programmet 'Kulturnyheterna'?");
+
+            if (allShows.Any(x => x.Title == "Kulturnyheterna"))
+                //if (allShows.Where(x=>x.Title=="Kulturnyheterna").Count() > 0) 
+                Console.WriteLine("Ja");
+            else
+                Console.WriteLine("Nej");
+
+            Header("Finns programmet 'Kulturnyheterna'? - oneliner");
+
+            Console.WriteLine(allShows.Any(x => x.Title == "Kulturnyheterna") ? "Ja" : "Nej");
+
+            Header("Finns programmet 'Ensam pappa söker'?");
+
+            if (allShows.Any(x => x.Title == "Ensam pappa söker"))
+                Console.WriteLine("Ja");
+            else
+                Console.WriteLine("Nej");
+
+            Header("Alla program som börjar kl 20.00");
+
+            foreach (Show show in allShows.Where(x => x.StartAt.TotalHours == 20))
+            //foreach (Show show in allShows.Where(x => x.StartAt == new TimeSpan(20,0,0)))
+            {
+                WriteInfoAboutShow(show);
+            }
+
+            Header("Alla programnamn med stora bokstäver");
+
+            foreach (string loudTitle in allShows.Select(x => x.Title.ToUpper()))
+            {
+                Console.WriteLine(loudTitle);
+            }
+
+            // Alla kanaler
+            Header("Alla kanaler");
+
+            foreach (string channel in allShows.Select(x => x.Channel).Distinct())
+            {
+                Console.WriteLine(channel);
+            }
+        }
+
+        // Extrauppgift
+        private static IEnumerable<Show> ParseTvShows_Linq(string[] rows)
+        {
+            return rows.Select(line => new Show
+            {
+                Channel = line.Split('*')[0],
+                StartAt = TimeSpan.Parse(line.Split('*')[1]),
+                Title = line.Split('*')[2],
+            });
         }
 
         private static IEnumerable<Show> ParseTvShows(string[] rows)
@@ -64,9 +175,16 @@ namespace CSharpCourse.Linq.Solutions
             return allShows;
         }
 
-        private static string[] ReadTvShowFile()
+        private static void Header(string text)
         {
-            return File.ReadAllLines(@"Linq\Solutions\tv-data.txt");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"\n{text.ToUpper()}\n");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        private static void WriteInfoAboutShow(Show show)
+        {
+            Console.WriteLine($"{show.Channel.PadRight(4)} {show.StartAt} {show.Title}");
         }
     }
 }
